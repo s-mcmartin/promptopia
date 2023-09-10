554ee6ec750112d139a32dee2b2ccfb6525cd755
s-mcmartin <shannoncmcguire@outlook.com>
2023-09-09 21:58:27 -0400

    first commit

---
+++ components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PromptCard from "./PromptCard";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="mt-2 prompt_layout">{desc}</p>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleEdit={() => handleEdit && handleEdit(post)}
+          handleDelete={() => handleDelete && handleDelete(post)}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Profile;
